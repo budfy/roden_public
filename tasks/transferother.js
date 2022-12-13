ff8cbed4d2e2f93e6daaece642dc986da0671550
budfy <serdyuk.ro@gmail.com>
2022-12-13 10:41:02 +0200

    Initial commit

---
+++ tasks/transferother.js
@@ -0,0 +1,11 @@
+const {
+  src,
+  dest
+} = require('gulp');
+const changed = require('gulp-changed');
+
+module.exports = function transferother() {
+  return src('src/**/*.!(html|php|scss|css|js|json|svg|ico|png|jpg|jpeg|webp|gif|ttf|woff|woff2|ttf2|eot)')
+    .pipe(changed('./docs/**/*.!(html|php|scss|css|js|json|svg|ico|png|jpg|jpeg|webp|gif|ttf|woff|woff2|ttf2|eot)'))
+    .pipe(dest('./docs/'))
+}
