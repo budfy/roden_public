c1345164f6a0d914196255b8da9fb3a13dfdc1a3
budfy <serdyuk.ro@gmail.com>
2023-01-12 17:23:00 +0200

    header

--- src/components/bem-blocks/topbar/_topbar.js
+++ src/components/bem-blocks/topbar/_topbar.js
@@ -2,16 +2,21 @@ function topbar() {
   let prevScrollpos = window.pageYOffset;
   window.addEventListener('scroll', function () {
     let currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos || prevScrollpos <= document.querySelector('.topbar').scrollHeight) {
+    if (prevScrollpos > currentScrollPos || prevScrollpos <= 350) {
       document.querySelector('.topbar').style.transform = 'translateY(0)';
     } else {
       document.querySelector('.topbar').style.transform = 'translateY(-100%)';
     }
-    if (currentScrollPos >= 500) {
+    if (currentScrollPos >= 250) {
       document.querySelector('.topbar__logo').style.width = "3.5rem";
       document.querySelector('.topbar__logo').style.height = "3.5rem";
-    } else if (currentScrollPos <= 400) {
-      document.querySelector('.topbar__logo').removeAttribute("style")
+      document.querySelector('.topbar').style.padding = "0.75rem 0";
+      document.querySelector('.topbar').style.boxShadow = "0 0.5rem 1.25rem rgba(0, 0, 0, 0.25)";
+      document.querySelector('.topbar__wrapper').style.alignItems = "center";
+    } else if (currentScrollPos <= 150) {
+      document.querySelector('.topbar, .topbar__logo, .topbar__wrapper').removeAttribute("style");
+      document.querySelector('.topbar__logo').removeAttribute("style");
+      document.querySelector('.topbar__wrapper').removeAttribute("style");
     }
     prevScrollpos = currentScrollPos;
   });
