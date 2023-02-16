41666587cdc10aadece53818e10bf741f8451b58
Roman Serdiuk <51324307+budfy@users.noreply.github.com>
2023-02-16 22:02:54 +0200

    header video added + topbar rebuilded

--- src/components/bem-blocks/topbar/_topbar.js
+++ src/components/bem-blocks/topbar/_topbar.js
@@ -1,5 +1,6 @@
 function topbar() {
   let prevScrollpos = window.pageYOffset;
+  let screenHeight = window.innerHeight;
   window.addEventListener('scroll', function () {
     let currentScrollPos = window.pageYOffset;
     // if (prevScrollpos > currentScrollPos || prevScrollpos <= 350) {
@@ -7,17 +8,20 @@ function topbar() {
     // } else {
     //   document.querySelector('.topbar').style.transform = 'translateY(-100%)';
     // }
-    if (currentScrollPos >= 150) {
-      document.querySelector('.topbar__logo').style.width = "3.5rem";
-      document.querySelector('.topbar__logo').style.height = "3.5rem";
-      document.querySelector('.topbar').style.padding = "0.75rem 0";
-      document.querySelector('.topbar').style.zIndex = 20;
-      document.querySelector('.topbar').style.boxShadow = "0 0.5rem 1.25rem rgba(0, 0, 0, 0.25)";
-      document.querySelector('.topbar__wrapper').style.alignItems = "center";
-    } else if (currentScrollPos < 100) {
-      document.querySelector('.topbar, .topbar__logo, .topbar__wrapper').removeAttribute("style");
-      document.querySelector('.topbar__logo').removeAttribute("style");
-      document.querySelector('.topbar__wrapper').removeAttribute("style");
+    if (currentScrollPos >= screenHeight) {
+      document.querySelector('.topbar.--main .topbar__logo').style.width = "3.5rem";
+      document.querySelector('.topbar.--main .topbar__logo').style.height = "3.5rem";
+      document.querySelector('.topbar.--main').style.padding = "0.75rem 0";
+      document.querySelector('.topbar.--main').style.opacity = 1;
+      document.querySelector('.topbar.--main').style.pointerEvents = "all";
+      document.querySelector('.topbar.--main').style.boxShadow = "0 0.5rem 1.25rem rgba(0, 0, 0, 0.25)";
+      document.querySelector('.topbar.--main .topbar__wrapper').style.alignItems = "center";
+    } else if (currentScrollPos < screenHeight) {
+      document.querySelector('.topbar.--main').style.opacity = 0;
+      document.querySelector('.topbar.--main').style.pointerEvents = "none";
+      document.querySelector('.topbar.--main, .topbar.--main .topbar__logo, .topbar__wrapper').removeAttribute("style");
+      document.querySelector('.topbar.--main .topbar__logo').removeAttribute("style");
+      document.querySelector('.topbar.--main .topbar__wrapper').removeAttribute("style");
     }
     prevScrollpos = currentScrollPos;
   });
