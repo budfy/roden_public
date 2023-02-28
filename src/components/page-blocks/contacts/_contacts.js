fb50f38cf982fa298210d37e494cff910a45d443
Roman Serdiuk <51324307+budfy@users.noreply.github.com>
2023-02-28 16:46:12 +0200

    Send_email_test_2

--- src/components/page-blocks/contacts/_contacts.js
+++ src/components/page-blocks/contacts/_contacts.js
@@ -27,4 +27,4 @@ function contactsForm() {
   }
 }
 
-contactsForm()
+contactsForm();
--- src/send.php
+++ src/send.php
@@ -1,45 +1,40 @@
 <?php
-function sendEmail($jsonData) {
-    $data = json_decode($jsonData, true);
+function sendEmail($formData) {
+    $toEmail = 'hello@roden.design';
+    $subject = 'Заявка з сайту від ' . $formData['name'];
+    
+    $boundary = md5(time());
 
-    // Перевірка обов'язкових полів
-    if (empty($data['name']) || empty($data['contact'])) {
-        return 'Ім\'я та контакт - обов\'язкові поля';
+    // headers
+    $headers = "From: " . $formData['name'] . "\r\n";
+    $headers .= "Reply-To: " . $formData['email'] . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: multipart/form-data; boundary=\"" . $boundary . "\"\r\n";
+    
+    // message content
+    $message = "Content-Type: text/plain; charset=\"utf-8\"\r\n";
+    $message .= "Content-Transfer-Encoding: 8bit\r\n\r\n";
+    $message .= "Ім'я: " . $formData['name'] . "\r\n";
+    $message .= "Посилання: " . $formData['link'] . "\r\n";
+    if (!empty($formData['message'])) {
+        $message .= "Повідомлення: " . $formData['message'] . "\r\n";
     }
-
-    // Встановлення параметрів для відправлення електронної пошти
-    $to = 'hello@roden.design';
-    $subject = 'Нова форма від '.$data['name'];
-    $message = 'Контактна інформація: '.$data['contact'];
-    $headers = array(
-        'From: '.$data['name'].' <'.$data['link'].'>',
-        'Reply-To: '.$data['contact'],
-        'Content-Type: text/html; charset=UTF-8'
-    );
-
-    // Додавання вкладення (якщо воно було прикріплено до форми)
-    if (!empty($_FILES['attachment']['tmp_name'])) {
-        $attachment = chunk_split(base64_encode(file_get_contents($_FILES['attachment']['tmp_name'])));
-        $filename = $_FILES['attachment']['name'];
-        $headers[] = 'MIME-Version: 1.0';
-        $headers[] = 'Content-Type: multipart/mixed; boundary="boundary"';
-        $message = "--boundary\r\n";
-        $message .= "Content-Type: text/html; charset=UTF-8\r\n";
-        $message .= "Content-Transfer-Encoding: 8bit\r\n\r\n";
-        $message .= $message."\r\n\r\n";
-        $message .= "--boundary\r\n";
-        $message .= "Content-Type: application/octet-stream; name=\"$filename\"\r\n";
+    
+    // file attachment
+    if (!empty($_FILES['file']['name'])) {
+        $attachment = chunk_split(base64_encode(file_get_contents($_FILES['file']['tmp_name'])));
+        $message .= "--" . $boundary . "\r\n";
+        $message .= "Content-Type: application/octet-stream; name=\"" . $_FILES['file']['name'] . "\"\r\n";
         $message .= "Content-Transfer-Encoding: base64\r\n";
-        $message .= "Content-Disposition: attachment; filename=\"$filename\"\r\n\r\n";
-        $message .= $attachment."\r\n\r\n";
-        $message .= "--boundary--";
+        $message .= "Content-Disposition: attachment\r\n\r\n";
+        $message .= $attachment . "\r\n";
     }
 
-    // Відправлення електронної пошти
-    if (mail($to, $subject, $message, implode("\r\n", $headers))) {
-        return 'Повідомлення надіслано';
+    // send email
+    if (mail($toEmail, $subject, $message, $headers)) {
+        echo "Повідомлення успішно надіслано.";
     } else {
-        return 'Виникла помилка при відправленні повідомлення';
+        echo "Помилка при відправленні повідомлення.";
     }
 }
 ?>
